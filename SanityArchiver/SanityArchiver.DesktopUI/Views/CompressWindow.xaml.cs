4f26b56de8f13d6f711d3e027c68d17c3ee2e165
Rasgacode <rongybaba12@gmail.com>
2020-03-04 22:11:35 +0100

    Implement CompressionWin + Handlers

---
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/CompressWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SanityArchiver.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for CompressWindow.xaml
+    /// </summary>
+    public partial class CompressWindow : Window
+    {
+        private MainWindow mW = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+
+        public CompressWindow()
+        {
+            InitializeComponent();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            mW.ctrMenuView.Status = false;
+            base.OnClosed(e);
+        }
+
+        private void Compress_KeyDown(object sender, RoutedEventArgs e)
+        {
+            //mW.ctrChildView
+        }
+
+        private void clickBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- SanityArchiver/SanityArchiver.DesktopUI/Views/MenuView.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MenuView.xaml.cs
@@ -20,9 +20,21 @@ namespace SanityArchiver.DesktopUI.Views
     /// </summary>
     public partial class MenuView : UserControl
     {
+        public bool Status { get; set; } = false;
+
         public MenuView()
         {
             InitializeComponent();
+        }
+
+        public void Compress(object sender, RoutedEventArgs e)
+        {
+            if (!Status)
+            {
+                CompressWindow cW = new CompressWindow();
+                cW.Show();
+                Status = true;
+            }
         }
     }
 }
