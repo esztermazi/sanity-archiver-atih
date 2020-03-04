9404347df968363e0da68643be8142f0bc175eab
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-03-04 16:19:58 +0100

    Implement rename text files with modal

--- SanityArchiver/SanityArchiver.DesktopUI/Views/RenameModal.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/RenameModal.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SanityArchiver.Application.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace SanityArchiver.DesktopUI.Views
 {
@@ -26,7 +29,21 @@ namespace SanityArchiver.DesktopUI.Views
 
         private void SaveRename(object sender, RoutedEventArgs e)
         {
-    
+            if (!string.IsNullOrEmpty(textField.Text))
+            {
+                try
+                {
+                    MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+                    CustomFile selectedFile = mainWindow.ctrChildView.MyDataGrid.SelectedItem as CustomFile;
+                    File.Move(selectedFile.Name, Path.GetDirectoryName(selectedFile.Name) + @"\"+ textField.Text + Path.GetExtension(selectedFile.Name));
+                    selectedFile.ShortName = textField.Text + Path.GetExtension(selectedFile.Name);
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "File does not exist in current context", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         private void CancelRename(object sender, RoutedEventArgs e)
