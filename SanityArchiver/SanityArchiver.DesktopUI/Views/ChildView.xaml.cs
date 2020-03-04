9a414a7a475e386a432163b2dbbe7c88118412b5
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-03-04 15:43:48 +0100

    Implement Add new text file in current folder

--- SanityArchiver/SanityArchiver.DesktopUI/Views/ChildView.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/ChildView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using SanityArchiver.Application.Models;
+using System;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -18,13 +21,37 @@ namespace SanityArchiver.DesktopUI.Views
             InitializeComponent();
         }
 
-        private void Set_ContextMenuContent(object sender, System.Windows.RoutedEventArgs e)
+        private void Set_ContextMenuContent(object sender, RoutedEventArgs e)
         {
-            if (MyDataGrid.SelectedItem == null) MyMenuItem.Header = "New";
+            if (MyDataGrid.SelectedItem == null)MyMenuItem.Header = "New Text File";
             else
             {
                 MyMenuItem.Header = "Rename";
             }
+        }
+
+        private void Excetute_ContextMenuCommand(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = e.OriginalSource as MenuItem;
+            if (menuItem.Header.Equals("New Text File")) AddFile();
+            else {
+                RenameFile();
+            }
+        }
+
+        private void AddFile()
+        {
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            CustomDirectory customDirectory = mainWindow.ctrTreeView.trvMenu.SelectedItem as CustomDirectory;
+            string newFilePath = $"{customDirectory.Name}" + @"\New file.txt";
+            File.CreateText(newFilePath);
+            CustomItem item = new CustomItem { ShortName = @"New file.txt", DateModified = DateTime.Now, Type = Path.GetExtension(newFilePath) };
+            MyDataGrid.Items.Add(item);
+        }
+
+        private void RenameFile()
+        {
+            MyDataGrid.SelectedItem = new TextBlock();
         }
 
         private void EscapePressed(object sender, KeyEventArgs e)
