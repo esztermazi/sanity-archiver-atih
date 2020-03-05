8dad716dcd9d51c43315b7e5b908ea8a44d0090c
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-05 20:21:01 +0100

    Exit button activation
    Co-Authored-By: rasgacode <rasgacode@users.noreply.github.com>
    Co-Authored-By: esztermazi <esztermazi@users.noreply.github.com>

--- SanityArchiver/SanityArchiver.DesktopUI/Views/MenuView.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MenuView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SanityArchiver.DesktopUI.Views
@@ -23,6 +24,12 @@ namespace SanityArchiver.DesktopUI.Views
                 cW.Show();
                 Status = true;
             }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mw.Close();
         }
     }
 }
